38cc262db62b3362a0a8ef1a4a3d0e5779994f1c
Jordi Marti <heedrox@gmail.com>
2021-12-27 14:27:22 +0100

    build rectangles by splitting randomly but only horizontally

--- src/rectangles-creator.js
+++ src/rectangles-creator.js
@@ -1,4 +1,27 @@
+import Rectangle from "./rectangle";
+
+const p = (x,y) => ({x,y});
+
 export default class RectanglesCreator {
-    constructor(width, height) {
+    constructor(width, height, randomProvider = Math.random) {
+        this.width = width;
+        this.height = height;
+        this.randomProvider = randomProvider;
+    }
+
+    build(numberElements) {
+        const p0 = p(0, 0);
+        const p1 = p(this.width, 0);
+        const p2 = p(0, this.height);
+        const p3 = p(this.width, this.height);
+        const rectangle = new Rectangle(p0, p1, p2, p3);
+        let rectangles = [ rectangle ];
+        for (let i = 1; i < numberElements; i++) {
+            const randomIndex = Math.floor(this.randomProvider() * rectangles.length);
+            const rectangle = rectangles[randomIndex];
+            const newTwoRectangles = rectangle.splitX();
+            rectangles.splice(randomIndex, 1, newTwoRectangles[0], newTwoRectangles[1]);
+        }
+        return rectangles;
     }
 };
--- src/rectangles-creator.spec.js
+++ src/rectangles-creator.spec.js
@@ -1,8 +1,78 @@
 import RectanglesCreator from "./rectangles-creator";
 
 describe('Rectangles Creator', () => {
-    it('should exist', () => {
-       new RectanglesCreator(100, 200);
-       expect(RectanglesCreator).toBeDefined();
+    it('inits', () => {
+       const builder = new RectanglesCreator(100, 200);
+       expect(builder.width).toBe(100);
+       expect(builder.height).toBe(200);
+    });
+
+    it('builds 1 rectangle', () => {
+        const builder = new RectanglesCreator(100, 200);
+
+        const rectangles = builder.build(1);
+
+        expect(rectangles.length).toBe(1);
+        expect(rectangles[0].p0.x).toBe(0);
+        expect(rectangles[0].p0.y).toBe(0);
+        expect(rectangles[0].p1.x).toBe(100);
+        expect(rectangles[0].p1.y).toBe(0);
+        expect(rectangles[0].p2.x).toBe(0);
+        expect(rectangles[0].p2.y).toBe(200);
+        expect(rectangles[0].p3.x).toBe(100);
+        expect(rectangles[0].p3.y).toBe(200);
+    });
+
+    it('builds 2 rectangles', () => {
+        const builder = new RectanglesCreator(100, 200);
+
+        const rectangles = builder.build(2);
+
+        expect(rectangles.length).toBe(2);
+    });
+
+    it('builds 3 rectangles', () => {
+        const builder = new RectanglesCreator(100, 200);
+
+        const rectangles = builder.build(3);
+
+        expect(rectangles.length).toBe(3);
+    });
+
+    it('builds 4 rectangles', () => {
+        const builder = new RectanglesCreator(100, 200);
+
+        const rectangles = builder.build(4);
+
+        expect(rectangles.length).toBe(4);
+    });
+
+    describe('splits randomly', () => {
+        it('case 1 - all first element', () => {
+            const mockRandom = jest.fn(() => 0);
+            const builder = new RectanglesCreator(100, 200, mockRandom);
+
+            const rectangles = builder.build(4);
+
+            expect(rectangles.length).toBe(4);
+            expect(rectangles[0].p0.x).toBe(0);
+            expect(rectangles[1].p0.x).toBe(12);
+            expect(rectangles[2].p0.x).toBe(25);
+            expect(rectangles[3].p0.x).toBe(50);
+
+        });
+
+        it('case 2 - all last element', () => {
+            const mockRandom = jest.fn(() => 0.999);
+            const builder = new RectanglesCreator(100, 200, mockRandom);
+
+            const rectangles = builder.build(4);
+
+            expect(rectangles.length).toBe(4);
+            expect(rectangles[0].p0.x).toBe(0);
+            expect(rectangles[1].p0.x).toBe(50);
+            expect(rectangles[2].p0.x).toBe(75);
+            expect(rectangles[3].p0.x).toBe(88);
+        });
     });
 });
